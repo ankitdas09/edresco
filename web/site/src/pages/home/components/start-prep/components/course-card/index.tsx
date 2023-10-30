590cca85ed6d23dd8bfe4d6ebfe5928d5fa61bd5
Ankit D <ankit.99d@gmail.com>
2023-10-30 23:37:14 +0530

    enhancements

--- web/site/src/pages/home/components/start-prep/components/course-card/index.tsx
+++ web/site/src/pages/home/components/start-prep/components/course-card/index.tsx
@@ -4,27 +4,33 @@ import "./styles/index.scss";
 interface PropTypes {
     text: string;
     class: string;
+    img: string;
 }
 
 const CourseCard = (props: PropTypes) => {
     return (
         <div className="course-card">
-            <Heading
-                text={props.text}
-                fontWeight={700}
-                font="Montserrat"
-                size="1.1rem"
-                maxWidth={100}
-                center={true}
-            />
-            <Heading
-                text={props.class}
-                fontWeight={400}
-                font="PT Sans"
-                size="0.75rem"
-                maxWidth={100}
-                center={true}
-            />
+            <div className="course-card-image-bottom">
+                <img src={props.img} alt="" className="img-fluid" />
+            </div>
+            <div className="course-card-text-top">
+                <Heading
+                    text={props.class}
+                    fontWeight={400}
+                    font="PT Sans"
+                    size="0.75rem"
+                    maxWidth={100}
+                    center={true}
+                />
+                <Heading
+                    text={props.text}
+                    fontWeight={700}
+                    font="Montserrat"
+                    size="1.1rem"
+                    maxWidth={100}
+                    center={true}
+                />
+            </div>
         </div>
     );
 };
--- web/site/src/pages/home/components/start-prep/index.tsx
+++ web/site/src/pages/home/components/start-prep/index.tsx
@@ -1,3 +1,4 @@
+import AbsIcon from "../../../../global/components/abs-icon";
 import Button from "../../../../global/components/btn";
 import Heading from "../../../../global/components/heading";
 import Space from "../../../../global/components/space";
@@ -6,14 +7,28 @@ import CourseCard from "./components/course-card";
 import "./styles/index.scss";
 
 const content = ["Disha", "Parishram", "Fresher", "Achiever", "Target"];
+const absIcons = [
+    { top: "0", left: "0px", icon: "./abs-icons/Group 380.png" },
+    { top: "20px", left: "240px", icon: "./abs-icons/abs-icon.png" },
+    { top: "40px", left: "480px", icon: "./abs-icons/abs-icon2.png" },
+    { top: "20px", left: "1260px", icon: "./abs-icons/Group (6).png" },
+    { top: "200px", left: "1260px", icon: "./abs-icons/Group (8).png" },
+    { top: "360px", left: "1100px", icon: "./abs-icons/Group (10).png" },
+    { top: "350px", left: "500px", icon: "./abs-icons/Group (11).png" },
+    { top: "380px", left: "200px", icon: "./abs-icons/Group (13).png" },
+    { top: "350px", left: "35px", icon: "./abs-icons/Group (7).png" },
+];
 
 const StartPrepSection = () => {
     return (
         <div className="start-prep-container container flex">
+            {absIcons.map((c) => {
+                return <AbsIcon top={c.top} left={c.left} icon={c.icon} />;
+            })}
             <div className="start-prep-left">
                 <Heading
                     text="Start the Preparations!"
-                    size="1.8rem"
+                    size="2rem"
                     fontWeight={600}
                     font="Montserrat"
                     maxWidth={1000}
@@ -58,13 +73,38 @@ const StartPrepSection = () => {
             <div className="start-prep-right">
                 <div className="flex justify-center align-center">
                     <div className="course-card-right-triple">
-                        <CourseCard text={content[0]} class="For class 9" key={0} />
-                        <CourseCard text={content[1]} class="For class 10" key={1} />
-                        <CourseCard text={content[2]} class="For class 11" key={2} />
+                        <CourseCard
+                            text={content[0]}
+                            class="For class 9"
+                            img="./course-icons/2.png"
+                            key={0}
+                        />
+                        <CourseCard
+                            text={content[1]}
+                            class="For class 10"
+                            img="./course-icons/3.png"
+                            key={1}
+                        />
+                        <CourseCard
+                            text={content[2]}
+                            class="For class 11"
+                            img="./course-icons/8.png"
+                            key={2}
+                        />
                     </div>
                     <div className="course-card-right-double">
-                        <CourseCard text={content[3]} class="For class 12" key={3} />
-                        <CourseCard text={content[4]} class="For Droppers" key={4} />
+                        <CourseCard
+                            text={content[3]}
+                            class="For class 12"
+                            img="./course-icons/1.png"
+                            key={3}
+                        />
+                        <CourseCard
+                            text={content[4]}
+                            class="For Droppers"
+                            img="./course-icons/6.png"
+                            key={4}
+                        />
                     </div>
                 </div>
             </div>
